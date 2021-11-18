59c634da0d8c9b2c1f6644d2926e392b4832d2da
João Vítor <soares.jvm@gmail.com>
2021-11-17 22:30:18 -0300

    Adição de testes unitários das classes de Situação e de Prontuário

---
+++ ManchesterProtocolIML.Tests/TestesProntuario.cs
@@ -0,0 +1,47 @@
+﻿using ManchesterProtocolML.Models;
+using ManchesterProtocolML.Validators;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using ManchesterProtocolML.Data;
+using FluentValidation.TestHelper;
+
+namespace ManchesterProtocolIML.Tests
+{
+    public class TestesProntuario
+    {
+        [Fact]
+        public void ProntuarioValido()
+        {
+            var prontuario = new Prontuario
+            (
+                id: Guid.NewGuid(),
+                frequenciaCardiaca: 80,
+                saturacaoOxigenio: 80,
+                frequenciaRespiratoria: 50,
+                sintomas: new List <Sintoma>()
+                {
+                    Sintomas.Teste
+                }
+             );
+            var validator = new ProntuarioValidator();
+
+            Assert.True(validator.TestValidate(prontuario).IsValid);
+        }
+        [Fact]
+        public void ProntuarioInvalido()
+        {
+            var prontuario = new Prontuario
+            (
+                id: Guid.NewGuid(),
+                frequenciaCardiaca: 300,
+                saturacaoOxigenio: -10,
+                frequenciaRespiratoria: 1000,
+                sintomas: new List<Sintoma>()
+             );
+            var validator = new ProntuarioValidator();
+
+            Assert.False(validator.TestValidate(prontuario).IsValid);
+        }
+    }
+}
---
+++ ManchesterProtocolIML.Tests/TestesSituacao.cs
@@ -0,0 +1,41 @@
+﻿using ManchesterProtocolML.Models;
+using ManchesterProtocolML.Validators;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using ManchesterProtocolML.Data;
+using FluentValidation.TestHelper;
+
+namespace ManchesterProtocolIML.Tests
+{
+    public class TestesSituacao
+    {
+        [Fact]
+        public void SituacaoValida()
+        {
+            var situacao = new Situacao
+            (
+                id: Guid.NewGuid(),
+                status: new Status(1, "Situacao"),
+                prioridade: new Prioridade(0, "Urgente")
+             );
+            var validator = new SituacaoValidator();
+
+            Assert.True(validator.TestValidate(situacao).IsValid);
+        }
+        [Fact]
+        public void SituacaoInvalida()
+        {
+            var situacao = new Situacao
+            (
+                id: Guid.NewGuid(),
+                status: null,
+                prioridade: null
+             );
+            var validator = new SituacaoValidator();
+
+            Assert.False(validator.TestValidate(situacao).IsValid);
+        }
+    }
+}
+
